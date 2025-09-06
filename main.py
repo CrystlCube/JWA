42ebe86bceeedcbb7e7cafa66ccddc4b9bdff69b
Grant Thompson <grantlewist@gmail.com>
2025-09-06 16:44:06 -0500

    Weekly dino update

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ try:
     current_dinos, needed_dinos = FileFunctions.create_dino_info()
 
     getting_info = GettingInfo(current_dinos, needed_dinos)
-    current_dinos, needed_dinos = getting_info.all_dino_info()
+    current_dinos, needed_dinos = getting_info.get_all_dino_info()
 
     unlocking_info = UnlockingInfo(current_dinos, needed_dinos)
-    current_dinos, needed_dinos = unlocking_info.all_dino_info()
+    current_dinos, needed_dinos = unlocking_info.get_all_dino_info()
 
     updating_info = UpdatingInfo(current_dinos, needed_dinos)
-    current_dinos, needed_dinos = updating_info.all_dino_info()
+    current_dinos, needed_dinos = updating_info.get_all_dino_info()
 
     FileFunctions.save_dino_info(current_dinos, needed_dinos)
 except:
